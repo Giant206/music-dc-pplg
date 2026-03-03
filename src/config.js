522b0d6d64373eba5034f340b3e443c2e1d73f84
gian. <abanggiant9@gmail.com>
2026-03-03 13:29:09 +0700

    Update All

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
    export default {
   TOKEN:
-    "TOKEN_BOT_DISCORD_LU",
+    "MTQ3MjYxNTM0NTQwMjU0NDEzOA.GM5j1G.xCBBqj4L4euxpKIF7XWnql5aRGKj-4xg4rROMI",
   MONGO:
-    "AMBIL_URL_MONGODB_LU",
+    "mongodb+srv://GianDatabase:Uvu06oN0QeYTFoGm@giandatabase.60zyiyh.mongodb.net/?appName=GianDatabase",
   OWNER: ["1186985440759267351"],
   PREFIX: "p!",
   COLOR: "#000000",
@@ -11,16 +11,24 @@
     ERROR: "<:error:796811149217648640>",
   }, 
   NODES: [
+
     {
       name: "Node 1",
+      url: "lava-v3.ajieblogs.eu.org:443",
+      auth: "https://dsc.gg/ajidevserver",
+      secure: true,
+    },
+
+    {
+      name: "Node 2",
       url: "lava-v3.millohost.my.id:443",
       auth: "https://discord.gg/mjS5J2K3ep",
       secure: true,
     },
 
     {
-      name: "Node 2",
-      url: "lava-v3.ajieblogs.eu.org:443",
+      name: "Node 3",
+      url: "lavalink.serenetia.com:443",
       auth: "https://dsc.gg/ajidevserver",
       secure: true,
     },
